4eac3a66197fed2c4388954f81de50be4fcd44c1
Roman R <Sakurasou2270@gmail.com>
2022-10-02 16:29:02 -0700

    git init

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import StoreInfo from "./components/StoreInfo";
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h1>Some Text</h1>
+      <StoreInfo />
+    </>
+  );
+}
+
+export default App;
